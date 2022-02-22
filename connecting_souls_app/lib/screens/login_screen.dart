957abb4efd847c04beb4cd4f0a718e49e3a234ce
Abhijna Raghavendra <a_raghavendra@ma.iitr.ac.in>
2022-02-22 23:04:42 +0530

    fixes overflow bug

--- connecting_souls_app/lib/screens/login_screen.dart
+++ connecting_souls_app/lib/screens/login_screen.dart
@@ -1,88 +1,92 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class LoginScreen extends StatelessWidget {
   const LoginScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Drawer(
-        child: CustomPaint(
-          painter: AppBarPainter(),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              const SizedBox(
-                height: 28.0,
-              ),
-              const Text(
-                'Connecting Souls',
-                style: TextStyle(
-                  fontSize: 34.0,
-                  fontFamily: 'Poppins',
-                  fontWeight: FontWeight.normal,
+    return ScreenUtilInit(
+      designSize: const Size(1080, 1920),
+      builder: () => MaterialApp(
+        home: Drawer(
+          child: CustomPaint(
+            painter: AppBarPainter(),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                SizedBox(
+                  height: 28.h,
                 ),
-              ),
-              const SizedBox(
-                height: 30.0,
-              ),
-              const Text(
-                  //' "The slogan comes here" ',
-                  'This application is for IIT-R Junta to help\n '
-                  'you connect to wellness center of IIT-R and\n'
-                  ' your fellow mates and seniors \n'
-                  '(anonymously) to prevent privacy issue \n'
-                  'while sharing anything.',
+                Text(
+                  'Connecting Souls',
                   style: TextStyle(
-                    fontSize: 19.0,
-                    fontFamily: 'Roboto',
-                    fontWeight: FontWeight.w400,
-                    color: Color.fromARGB(400, 11, 10, 10),
+                    fontSize: 34.sp,
+                    fontFamily: 'Poppins',
+                    fontWeight: FontWeight.normal,
                   ),
-                  textAlign: TextAlign.center),
-              const SizedBox(
-                height: 9.0,
-              ),
-              Image.asset(
-                'assets/1.png',
-                width: 283.0,
-              ),
-              SizedBox(
-                width: 302.0,
-                child: TextButton(
-                  onPressed: () {},
-                  style: ButtonStyle(
-                    backgroundColor: MaterialStateProperty.all(
-                        const Color.fromARGB(500, 151, 227, 154)),
-                    shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                      RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(12.0),
+                ),
+                SizedBox(
+                  height: 30.sp,
+                ),
+                Text(
+                    //' "The slogan comes here" ',
+                    'This application is for IIT-R Junta to help\n '
+                    'you connect to wellness center of IIT-R and\n'
+                    ' your fellow mates and seniors \n'
+                    '(anonymously) to prevent privacy issue \n'
+                    'while sharing anything.',
+                    style: TextStyle(
+                      fontSize: 19.sp,
+                      fontFamily: 'Roboto',
+                      fontWeight: FontWeight.w400,
+                      color: const Color.fromARGB(400, 11, 10, 10),
+                    ),
+                    textAlign: TextAlign.center),
+                SizedBox(
+                  height: 9.h,
+                ),
+                Image.asset(
+                  'assets/1.png',
+                  width: 283.w,
+                ),
+                SizedBox(
+                  width: 302.w,
+                  child: TextButton(
+                    onPressed: () {},
+                    style: ButtonStyle(
+                      backgroundColor: MaterialStateProperty.all(
+                          const Color.fromARGB(500, 151, 227, 154)),
+                      shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                        RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(12.r),
+                        ),
                       ),
                     ),
-                  ),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      const Text(
-                        'Login with Google',
-                        style: TextStyle(
-                            fontSize: 23.0,
-                            fontFamily: 'Roboto',
-                            color: Color.fromARGB(450, 23, 22, 22),
-                            fontWeight: FontWeight.w500),
-                      ),
-                      const SizedBox(
-                        width: 7,
-                      ),
-                      Image.asset(
-                        'assets/2.png',
-                        width: 36.0,
-                      ),
-                    ],
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          'Login with Google',
+                          style: TextStyle(
+                              fontSize: 23.sp,
+                              fontFamily: 'Roboto',
+                              color: const Color.fromARGB(450, 23, 22, 22),
+                              fontWeight: FontWeight.w500),
+                        ),
+                        SizedBox(
+                          width: 7.w,
+                        ),
+                        Image.asset(
+                          'assets/2.png',
+                          width: 36.w,
+                        ),
+                      ],
+                    ),
                   ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       ),
