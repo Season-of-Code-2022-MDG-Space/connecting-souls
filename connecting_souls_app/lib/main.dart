610e61a182b5068cae27b5fdd2f0b4dc5d820a74
Abhijna Raghavendra <a_raghavendra@ma.iitr.ac.in>
2022-02-23 13:45:45 +0530

    bug in multiple providers

--- connecting_souls_app/lib/main.dart
+++ connecting_souls_app/lib/main.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:connecting_souls_app/screens/login_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
+Future main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(MaterialApp(
     routes: {
       '/': (context) => const LoginScreen(),
